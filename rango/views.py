3818b1d9a703ce07e5a44289d4eafcaa2a6664ec
Mingyang Chen <2512330C@student.gla.ac.uk>
2022-02-03 19:42:20 +0800

    unit4

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("""Rango says hey there partner! <a href="/rango/about">About</a>""")
-def about(requedt):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html',context =context_dict)
+def about(request):
+    context_dict = {'boldmessage': 'Mingyang Chen'}
+    return render(request, 'rango/about.html',context =context_dict)
