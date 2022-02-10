3a21f9f51544f06426b07aa9e005971c79d4b587
Mingyang Chen <2512330C@student.gla.ac.uk>
2022-02-11 06:24:39 +0800

    chapeter 9

--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,8 @@
+from distutils.command.upload import upload
 from turtle import title
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -23,3 +25,11 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
